78ecaa77b295863f2e3bce1b0f1274132d67ae8f
Alessandro <alessandro.piccione.75@gmail.com>
2020-05-24 16:38:58 +0100

    Remove default pages and settings and create Probability  page.

--- WebApplication/ClientApp/src/app/app.module.ts
+++ WebApplication/ClientApp/src/app/app.module.ts
@@ -1,22 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+import { RouterModule } from '@angular/router'
 
-import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
-import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { AppComponent } from './app.component'
+import { NavMenuComponent } from './nav-menu/nav-menu.component'
+import { HomeComponent } from './home/home.component'
+import { ProbabilityPage } from "./probability/probability.page"
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
+    ProbabilityPage
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -24,8 +22,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'probability', component: ProbabilityPage },
+      { path: '**', redirectTo: '' }
     ])
   ],
   providers: [],
---
+++ WebApplication/ClientApp/src/app/probability/probability.page.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core"
+
+@Component({
+  selector: "probability-page",
+  templateUrl: "./probability.page.html",
+  styleUrls: ["./probability.page.css"]
+})
+export class ProbabilityPage {
+
+
+
+}
