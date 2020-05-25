6bb7a15c320b6e0cdabcbf3c07e2eca2a767ea5f
Alessandro <alessandro.piccione.75@gmail.com>
2020-05-25 02:04:33 +0100

    (missing attribute)

--- UnitTests/Main/CalculateProbabilityInput.test.fs
+++ UnitTests/Main/CalculateProbabilityInput.test.fs
@@ -5,7 +5,7 @@ open NUnit.Framework
 open FsUnit
 open Probability.Core.Models
 
-
+[<Test>]
 let FromCalculateProbabilityRequest() =
     let request = Test_utils.createRequest 1m 2m "type"
     let input = CalculateProbabilityInput.FromCalculateProbabilityRequest(request)
